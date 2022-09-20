40e352db968384671c8b1baac9b1db4fb3ce79da
CiroWen <cirowencs@gmail.com>
2022-09-20 11:35:27 -0700

    basic setup for SortingVisualizer component, displays all the generated random number in the array

--- src/SourtingVisualizer/SortingVisualizer.jsx
+++ src/SourtingVisualizer/SortingVisualizer.jsx
@@ -1,4 +1,48 @@
 import React from "react";
 import "./SortingVisualizer.css";
 
-export default class SortingVisualizer extends React.Component {}
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // variable stored in state
+    this.state = {
+      array: [], //main array to store random generated  numbers
+    };
+  }
+
+  /**
+   * excutes when SortingVisualizer component loads
+   */
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  /**
+   * method to generate and reset array
+   */
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < 100; i++) {
+      array.push(randomIntFromInterval(5, 1000));
+    }
+    this.setState({ array });
+  }
+
+  render() {
+    const { array } = this.state;
+    return (
+      <>
+        {array.map((value, idx) => (
+          <div className="array-bar" key={idx}>
+            {value}
+          </div>
+        ))}
+      </>
+    );
+  }
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * max + min);
+}
