f329a810123af881901f6f08a84f6eeb7139dfbd
CiroWen <cirowencs@gmail.com>
2022-09-24 22:31:16 -0700

    animation implemented

--- src/SourtingVisualizer/SortingVisualizer.jsx
+++ src/SourtingVisualizer/SortingVisualizer.jsx
@@ -3,7 +3,8 @@ import "./SortingVisualizer.css";
 import * as algos from "./sortingAlgos/sortingAlgos";
 const App = props => {
   const [arr, setArr] = useState([]); //variable stored in state, useState returns the current state and a function that updates it.
-  const [barArr, setBarArr] = useState([])
+  // const [barArr, setBarArr] = useState([]);
+  let recordArr = [];
   /**
    *
    * @param {number} min inclusive lower bound
@@ -16,7 +17,7 @@ const App = props => {
   /**
    * method to generate an array containing random numbers
    */
-  const resetArr = async() => {
+  const resetArr = () => {
     const temp = [];
     for (let i = 0; i < 130; i++) {
       let rnd;
@@ -25,22 +26,59 @@ const App = props => {
       } while (temp.includes(rnd));
       temp.push(rnd);
     }
-     await setArr(temp);
-    setBarArr(document.querySelectorAll('.array-bar'))
+    setArr(temp);
+    // setBarArr(document.querySelectorAll(".array-bar"));
+  };
+
+  const recordedAnimation = () => {
+    for (let i = 0; i< recordArr.length; i++){
+      const arrayBars = document.getElementsByClassName('array-bar')
+      const isClrChanged = i % 3 !== 2;
+      if(isClrChanged){
+        const [barOneIdx , barTwoIdx] = recordArr[i]
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? 'blue' : '#ff7782'
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * 10);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = recordArr[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * 10);
+      }
+      
+    }
+
   };
 
   const mergeSort = () => {
-    
     const jsSortedArr = arr.slice().sort((a, b) => a - b);
-    const selfSortedArr = algos.mergeSort(Array.prototype.slice.call(barArr))
-    console.log(jsSortedArr);
-    const tempArr = []
-    selfSortedArr.map((val,idx) => {
-      tempArr.push(parseInt(val.style.height.replace('px','')))
-    })
-    console.log(isEqual(jsSortedArr,tempArr));
+    const selfSortedArr = algos.mergeSort(
+      arr,
+      arr.slice(),
+      // Array.prototype.slice.call(barArr),
+      // Array.prototype.slice.call(barArr),
+      recordArr
+    );
+      recordedAnimation()
+    // const tempArr = [];
+    // console.log(selfSortedArr);
+    // selfSortedArr.map((val, idx) => {
+    //   tempArr.push(parseInt(val.style.height.replace("px", "")));
+    // });
+    // console.log(isEqual(jsSortedArr, tempArr));
+    console.log(isEqual(jsSortedArr, selfSortedArr));
     // setArr(tempArr)
+    // setArr(selfSortedArr);
+    console.log(recordArr);
+    // console.log(tempArr);
+    
   };
+  
 
   const isEqual = (arrA, arrB) => {
     if (arrA.length !== arrB.length) return false;
--- src/SourtingVisualizer/sortingAlgos/sortingAlgos.js
+++ src/SourtingVisualizer/sortingAlgos/sortingAlgos.js
@@ -1,41 +1,107 @@
-export const mergeSort = arr => {
+export const mergeSort = (arr, auxiliaryArr, trackerArr) => {
+  // console.log(ogArr);
   if (arr.length < 2) return arr; // if the array has only one element, no need for sorting
   const midIdx = Math.floor(arr.length / 2); //middle idx
-  const left = mergeSort(arr.slice(0, midIdx)); //shallow copy [0,midIdx)
-  const right = mergeSort(arr.slice(midIdx)); //shallow copy [midIdx, ...arr]
+  const left = mergeSort(arr.slice(0, midIdx), auxiliaryArr, trackerArr); //shallow copy [0,midIdx)
+  const right = mergeSort(arr.slice(midIdx), auxiliaryArr, trackerArr); //shallow copy [midIdx, ...arr]
   const resultArr = [];
   let i = 0,
+    m = 0,
     k = 0;
-
+  // console.log(auxiliaryArr);
   // Compares and pushes the smaller element to result array
   while (i < left.length && k < right.length) {
-    const leftHeight = parseInt(left[i].style.height.replace("px", ""));
-    const rightHeight = parseInt(right[k].style.height.replace("px", ""));
+    // const leftHeight = parseInt(left[i].style.height.replace("px", ""));
+    // const rightHeight = parseInt(right[k].style.height.replace("px", ""));
+    trackerArr.push([
+      auxiliaryArr.indexOf(left[i]),
+      auxiliaryArr.indexOf(right[k]),
+    ]);
+    trackerArr.push([
+      auxiliaryArr.indexOf(left[i]),
+      auxiliaryArr.indexOf(right[k]),
+    ]);
 
-    let m = i,
-      n = k;
     // debugger;
-    setTimeout(() => {
-      left[m].style.backgroundColor = "blue";
-      right[n].style.backgroundColor = "blue";
-      setTimeout(() => {
-        left[m].style.backgroundColor = "#ff7782";
-        right[n].style.backgroundColor = "#ff7782";
-      }, m + 1 * 10);
-    }, m * 10);
-    if (leftHeight <= rightHeight) {
+    // setTimeout(() => {
+    //   left[m].style.backgroundColor = "blue";
+    //   right[n].style.backgroundColor = "blue";
+    //   setTimeout(() => {
+    //     left[m].style.backgroundColor = "#ff7782";
+    //     right[n].style.backgroundColor = "#ff7782";
+    //   }, m + 1 * 10);
+    // }, m * 10);
+    // debugger
+    if (left[i] < right[k]) {
       resultArr.push(left[i]);
+      trackerArr.push([
+        auxiliaryArr.indexOf(arr[m++]),
+        auxiliaryArr[auxiliaryArr.indexOf(left[i])],
+      ]);
+      // debugger
+      // console.log(left[i],right[k]);
+      // find the original index of the element, then swap them
+      // console.log(arr.indexOf(left[i]));
+      // console.log(arr[arr.indexOf(right[k])].style.height);
+      // setTimeout(() => {
+      //   //   console.log(arr.indexOf(left[i]));
+      //   // console.log(arr[arr.indexOf(right[k])].style.height);
+      //   ogArr[ogArr.indexOf(left[m])].style.height = `${rightHeight}px`;
+      //   ogArr[ogArr.indexOf(right[n])].style.height = `${leftHeight}px`;
+      // }, m + 1 * 10);
       i++;
     } else {
+      // debugger
       resultArr.push(right[k]);
+      trackerArr.push([
+        auxiliaryArr.indexOf(arr[m++]),
+        auxiliaryArr[auxiliaryArr.indexOf(right[k])],
+      ]);
+      // setTimeout(() => {
+      //   //   console.log(arr.indexOf(left[i]));
+      //   // console.log(arr[arr.indexOf(right[k])].style.height);
+      //   ogArr[ogArr.indexOf(left[m])].style.hei ght = `${rightHeight}px`;
+      //   ogArr[ogArr.indexOf(right[n])].style.height = `${leftHeight}px`;
+      // }, n + 1 * 10);
       k++;
     }
   }
 
   //   after comparing loop, push all the remaining elements to result array
-  while (i < left.length) resultArr.push(left[i++]);
-  while (k < right.length) resultArr.push(right[k++]);
+  while (i < left.length) {
+    trackerArr.push([
+      auxiliaryArr.indexOf(left[i]),
+      auxiliaryArr.indexOf(left[i]),
+    ]);
+    trackerArr.push([
+      auxiliaryArr.indexOf(left[i]),
+      auxiliaryArr.indexOf(left[i]),
+    ]);
+    trackerArr.push([
+      auxiliaryArr.indexOf(arr[m++]),
+      auxiliaryArr[auxiliaryArr.indexOf(left[i])],
+    ]);
+    resultArr.push(left[i++]);
+  }
+  while (k < right.length) {
+    trackerArr.push([
+      auxiliaryArr.indexOf(right[k]),
+      auxiliaryArr.indexOf(right[k]),
+    ]);
+    trackerArr.push([
+      auxiliaryArr.indexOf(right[k]),
+      auxiliaryArr.indexOf(right[k]),
+    ]);
+    trackerArr.push([
+      auxiliaryArr.indexOf(arr[m++]),
+      auxiliaryArr[auxiliaryArr.indexOf(right[k])],
+    ]);
 
+    resultArr.push(right[k++]);
+  }
+
+  // console.log(resultArr);
+  // console.log(arr);
   return resultArr;
 
   //   if (arr.length === 1 || arr.length === 0) return arr; // if the array has only one element, no need for sorting
